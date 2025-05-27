74f2b1a851947f3beb0aeeb28c706b4767f280f8
zoliad.17 <61303548+zoliad17@users.noreply.github.com>
2025-05-27 21:27:14 +0800

    added a connection to superbase src/Supabase/supabaseTest.js

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+
 //import App from './App.tsx'
-import OtpPage from './components/OTP/otpPage'
+//import OtpPage from './components/OTP/otpPage'//
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
   {/* <App /> */}
-    <OtpPage />
   </StrictMode>,
 )
